36c66e659a911a790ee6fd509e4773f2b2f8ba82
Ionut <barborionut15@gmail.com>
2025-07-20 21:01:06 +0300

    Started to work on the structure

---
+++ src/image_process.rs
@@ -0,0 +1,30 @@
+use vtracer::{ColorMode, Config, Hierarchical};
+
+use crate::vtracer::{Config, ColorMode, Hierarchical, PathSimplifyMode};
+
+fn convert_to_json()  {
+
+    let config = Config {
+        color_mode: ColorMode::Color,
+        hierarchical: Hierarchical::Cutout,
+        filter_speckle: 4,
+        color_precision: 6,
+        layer_difference: 16,
+        mode: PathSimplifyMode::Polygon,
+        corner_threshold: 60,
+        length_threshold: 4.0,
+        max_iterations: 5,
+        splice_threshold: 45,
+        path_precision: Some(2),
+    };
+}
+
+fn parse_svg(arg: &str) -> &str {
+    todo!();
+}
+fn write_to_json(arg: &str) -> &str {
+    todo!();
+}
+fn post_to_cli(arg: &str) -> &str {
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use crate::vtracer;
+
+mod image_process;
+
+#[inline]
+pub fn Convert_image_to_svg() {
+    todo!();
 }
+fn main() {}
